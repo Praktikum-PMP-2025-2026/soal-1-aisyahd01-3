832b3b62021586a0a9eea8c5b704363358e8b04f
aisyahd01 <aisyahdahayu02@gmail.com>
2026-05-11 08:59:33 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,57 @@
 #include <stdio.h>
 
-int main(void) {
+int N;
+int matrix[100][100];
+int degree [100];
+
+void degreeandMax(){
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
+            degree[i]=0;
+            if (matrix[i][j]==1){
+            degree[i]+=1;
+        }
+    }
+
+    for (int i=0; i<N; i++){ 
+        printf("DEGREE %d %d\n", &i, &degree[i]);
+    }
+
+    int index=0, maxvertex=0;
+    for (int i=0; i<N; i++);
+        if (degree[i]>maxvertex){
+            maxvertex=degree[i];
+            index=i;
+        }
+        printf("MAX_VERTEX %d\n", &index);
+    }
+}
+
+void isolatedNode(){
+    int found=0;
+    for (int i=0; i<N; i++){
+        if (degree[i]==0){
+            printf("ISOLATED %d\n", i);
+            found=1;
+        }
+    }
+
+    if (!found){
+        printf("ISOLATED 0");
+    }
+
+}
+    
+
+int main(){
+    scanf ("%d", &N);
+    for (int i=0; i<N; i++){
+        for (int j=0; i<N; j++){
+            scanf("%d", &matrix[i][j]);
+        }  
+    }
+    degreeandMax();
+    isolatedNode();
     return 0;
 }
+
